fab27257b954176b304a33d4da56fb6092b0b34a
Vladimir Bozhkov <vl.bozhkov@gmail.com>
2019-03-27 13:38:08 +0200

    finished wherefore art thou challenge

---
+++ wherefore_art_thou.js
@@ -0,0 +1,56 @@
+// --------------Challenge--------------------
+// Make a function that looks through an array of objects (first argument) and returns
+// an array of all objects that have matching name and value pairs (second argument).
+// Each name and value pair of the source object has to be present
+//  in the object from the collection if it is to be included in the returned array.
+
+// For example, if the first argument is [{ first: "Romeo", last: "Montague" },
+// { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }],
+// and the second argument is { last: "Capulet" },
+// then you must return the third object from the array (the first argument),
+// because it contains the name and its value, that was passed on as the second argument.
+
+function whatIsInAName(collection, source) {
+  // turn source object to array of keys
+  let srcKeys = Object.keys(source);
+  // loop through collection objects
+  let arr = collection.filter(function(obj) {
+    // if there no propery in collection and object not equal to source
+    for (let i = 0; i < srcKeys.length; i++) {
+      if (
+        !obj.hasOwnProperty(srcKeys[i]) ||
+        obj[srcKeys[i]] !== source[srcKeys[i]]
+      ) {
+        //then return false
+        return false;
+      }
+    }
+    // otherwise return to arr
+    return true;
+  });
+  return arr;
+}
+whatIsInAName(
+  [
+    { apple: 1, bat: 2 },
+    { apple: 1 },
+    { apple: 1, bat: 2, cookie: 2 },
+    { bat: 2 }
+  ],
+  { apple: 1, bat: 2 }
+); //should return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie":2 }]
+// whatIsInAName(
+//   [
+//     { first: 'Romeo', last: 'Montague' },
+//     { first: 'Mercutio', last: null },
+//     { first: 'Tybalt', last: 'Capulet' }
+//   ],
+//   { last: 'Capulet' }
+// );
+// ----------------Tests---------------
+// whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }) should return [{ first: "Tybalt", last: "Capulet" }].
+// whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 }) should return [{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }].
+// whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }) should return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }].
+// whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 }) should return [{ "apple": 1, "bat": 2, "cookie": 2 }].
+// whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }, { "bat":2 }], { "apple": 1, "bat": 2 }) should return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie":2 }].
+// whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3}) should return []
